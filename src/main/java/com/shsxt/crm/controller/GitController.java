8b24d181a241f3640fceb91011eea0db89e51cd5
zxg <2284533735@qq.com>
2020-11-06 16:39:43 +0800

    更新GitController03

--- src/main/java/com/shsxt/crm/controller/GitController.java
+++ src/main/java/com/shsxt/crm/controller/GitController.java
@@ -1,4 +1,13 @@
 package com.shsxt.crm.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class GitController {
+
+    @RequestMapping
+    @ResponseBody
+    public void query(){
+        //本地修改
+    }
 }
